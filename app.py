cfa69d262ef38e4d8ef3aec114ec454b06398b73
Hunain546 <93545946+Hunain546@users.noreply.github.com>
2025-01-17 03:30:38 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # API Configuration
-API_BASE_URL = "https://f9c4e0d1-a0ef-428f-ac08-419a8a7b2a56-00-1ufezh4ntjwmk.sisko.replit.dev/"  # Replace with your FastAPI server's URL
+API_BASE_URL = "https://f9c4e0d1-a0ef-428f-ac08-419a8a7b2a56-00-1ufezh4ntjwmk.sisko.replit.dev"  # Replace with your FastAPI server's URL
 
 # Page Configuration
 st.set_page_config(page_title="O/Adapt - AI Learning Assistant", page_icon="📝", layout="wide")
